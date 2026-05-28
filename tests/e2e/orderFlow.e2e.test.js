2d92130399f35be31fb95f8c9802e80f3d7e07ba
Robert Ashfield <rashfield959@gmail.com>
2026-05-28 18:04:24 +0100

    replaced hardcoded timeout with waiting on order to change from pending
    minor refactoring also

--- tests/e2e/orderFlow.e2e.test.js
+++ tests/e2e/orderFlow.e2e.test.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-jest.setTimeout(15000);
+jest.setTimeout(15000); // increase timeout for async processing and service responses
 
 describe("Order flow E2E", () => {
   test("creates order and updates status", async () => {
@@ -9,19 +9,23 @@ describe("Order flow E2E", () => {
       "http://localhost:3001/orders",
       { amount: 50 }
     );
-
     expect(createRes.status).toBe(200);
 
-    const orderId = createRes.data.order.orderId;
+    let order = createRes.data.order;
+    const orderId = order.orderId;
 
-    // wait for async event processing
-    await new Promise(resolve => setTimeout(resolve, 7000));
+    // wait for order status to change from pending
+    while (order.status == "pending") {
+        
+        const res = await getOrder(orderId);
+        order = res.data;
+
+        // wait for async event processing
+        await new Promise(resolve => setTimeout(resolve, 500));
+    }
 
     // fetch updated order
-    const getRes = await axios.get(
-      `http://localhost:3001/orders/${orderId}`
-    );
-
+    const getRes = await getOrder(orderId);
     expect(getRes.status).toBe(200);
 
     expect(["paid", "failed"]).toContain(
@@ -29,3 +33,9 @@ describe("Order flow E2E", () => {
     );
   });
 });
+
+function getOrder(orderId) {
+    return axios.get(
+        `http://localhost:3001/orders/${orderId}`
+    );
+}
